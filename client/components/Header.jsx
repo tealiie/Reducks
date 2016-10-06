e8dab3f2e4f2b3b714c7fb3e820dc9cd8f37c75c
Sach <shsaci@yahoo.com.au>
2016-10-07 10:13:57 +1300

    added three components for Reducks

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,10 +3,8 @@ import React from 'react'
 export default React.createClass({
   render(){
     return(
-      <div className="headerWrapper text-center">
-        QuizzApp
-        <br/>
-        <h2> Answer true or false to the following </h2>
+      <div className="header">
+        <h2>Reducks!</h2>
       </div>
     )
   }
